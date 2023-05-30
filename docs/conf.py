95eeaa71437a10059878792a590e2c4016e5e465
tkatchen <tkatchen7@gmail.com>
2023-05-29 21:22:42 -0400

    Maybe?

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath('../src/lodepy'))
-
 project = 'lodepy'
 copyright = '2023, Tyler Katchen'
 author = 'Tyler Katchen'
