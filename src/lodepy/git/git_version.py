e5d665894f998f503bdd31854d591a5f6a6f1ced
Tyler Katchen <tkatchen7@gmail.com>
2023-06-30 01:04:11 -0400

    Add git version, other version changes (#18)
    * Fix version, add GitVersion
    
    * Oops, add version

--- src/lodepy/git/git_version.py
+++ src/lodepy/git/git_version.py
@@ -2,21 +2,25 @@ from typing import TypeVar
 from lodepy.handling.log_error import LogError
 from lodepy.nodes.node import Node
 from lodepy.nodes.node_return import NodeReturn
+from lodepy.core.version import Version
 from lodepy.tasks.task import Task
 from lodepy.handling.log_manager import LogManager
 
 K = TypeVar('K')
 
-class GitStatus(Task):
+class GitVersion(Task):
     def __init__(self, **kwargs):
         self.ssh = True
         if 'ssh' in kwargs:
             self.ssh = kwargs['ssh']
 
-    def execute(self, node: 'Node') -> 'NodeReturn[str]':
-        res = node.executor.send_command('git status', ssh=self.ssh)
+    def execute(self, node: 'Node') -> 'NodeReturn[Version]':
+        res = node.executor.send_command('git version', ssh=self.ssh)
 
         if res[2] != '':
-            LogManager.add_log(LogError('git status', res[2], node))
+            LogManager.add_log(LogError('git version', res[2], node))
 
-        return NodeReturn(node, res[1], res[0])
+        print(res)
+
+        return NodeReturn(node, Version(res[1].split(' ')[2].strip()), res[0])
+
--- src/lodepy/nodes/node_return.py
+++ src/lodepy/nodes/node_return.py
@@ -1,17 +1,18 @@
-from typing import TypeVar
+from typing import TypeVar, Union
 
 from lodepy.nodes.node import Node
-
+from lodepy.core.comparable import Comparable
 K = TypeVar('K')
 
-class NodeReturn(Node):
+class NodeReturn(Node, Comparable):
     '''
     The return of a task executed on a certain node.
     '''
     def __init__(self, node: Node, value: K, returncode: int):
         #pylint: disable=super-init-not-called
-        super().copy(node)
+        super(NodeReturn, self).copy(node)
         self.value = value
+        self.comp = self._handle_comp
         self.returncode = returncode
 
     def __str__(self) -> K:
@@ -22,6 +23,27 @@ class NodeReturn(Node):
             return self.value == other.value
         else:
             return self.value == other
+
+    def _handle_comp(self, op: str, to_comp: Union[K,'NodeReturn[K]']) -> bool: 
+        '''
+        Handler function for the filters from comparisons.
+        '''
+        if type(to_comp) == type(self):
+            if op == '<': return self.value < to_comp.value
+            elif op == '>': return self.value > to_comp.value
+            elif op == '==': return self.value == to_comp.value
+            elif op == '!=': return self.value != to_comp.value
+            elif op == '<=': return self.value <= to_comp.value
+            elif op == '>=': return self.value >= to_comp.value
+            else: raise LodepyInvalidComparison(op)
+                
+        if op == '<': return self.value < to_comp
+        elif op == '>': return self.value > to_comp
+        elif op == '==': return self.value == to_comp
+        elif op == '!=': return self.value != to_comp
+        elif op == '<=': return self.value <= to_comp
+        elif op == '>=': return self.value >= to_comp
+        else: raise LodepyInvalidComparison(op) 
 
     def __hash__(self) -> int:
         return super().__hash__()
